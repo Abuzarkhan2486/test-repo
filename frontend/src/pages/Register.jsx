2524b0535554cbcce60c70704cdb92cdd20f73de
Your New Username <abuzarkhan2486@gmail.com>
2026-04-27 14:24:12 +0530

    abc

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div> hello today i try to use github </div>
+    <div> hello today i try to use github gngjhjklaz </div>
   )
 }
 
